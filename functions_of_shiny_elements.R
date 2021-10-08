93b6d4aae89152f6d5ee19ffd8a7355bbf6aa039
hsc-dsvm-3\dsuser <aaron.gorman@sibni.org>
2021-10-08 16:50:06 +0100

    collecting R functions

---
+++ functions_of_shiny_elements.R
@@ -0,0 +1,18 @@
+vbox <- function(label='label',textid='textOutput_here',col='rgba(60,130,180,0.5)'){
+   div(style =
+          "border-radius: 15px;
+                      border-style: solid;
+                      border-color:white;
+            padding:0px;
+            margin:0px 10px;
+            color:white;
+                  font-weight: 300;
+                  text-color:blue;
+                  text-align:center;
+                  background-color:{col};
+            ",
+       h5(label,style='color:white;'),
+       h2(style='font-weight:bold;display: inline-block;vertical-align:top;color:white;',
+          textid
+       ))
+}
